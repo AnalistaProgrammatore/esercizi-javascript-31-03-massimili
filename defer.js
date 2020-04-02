21a1972afe3ae6a6aa579b7820cb5394fe85bdc4
Massimiliano Puca <max.puca@gmail.com>
2020-04-02 19:28:30 +0200

    defer

--- defer.js
+++ defer.js
@@ -1,1 +1,12 @@
+class FunzioneEstesa extends Function {
+    defer(ms) {
+        // return setTimeout(console.log("Ciao"), ms);
+        setTimeout(() => {
+            console.log("ciao in ritardo di " + ms + " millisecondi")
+        }, ms);
+    }
+}
+
+const ritarda = new FunzioneEstesa()
+ritarda.defer(3000)
 
